bb5547d44058eb77a279d9f9741decf1b4cf22f3
motivate <motivatedmind19@gmail.com>
2021-04-11 11:57:21 +0530

    first working version of calculator soap service

--- src/main/java/com/javatechie/spring/soap/api/service/LoanEligibilityService.java
+++ src/main/java/com/javatechie/spring/soap/api/service/LoanEligibilityService.java
@@ -11,28 +11,17 @@ import com.javatechie.spring.soap.api.loaneligibility.CustomerRequest;
 public class LoanEligibilityService {
 
 	public Acknowledgement checkLoanEligibility(CustomerRequest request) {
-		Acknowledgement acknowledgement = new Acknowledgement();
-		List<String> mismatchCriteriaList = acknowledgement.getCriteriaMismatch();
-
-		if (!(request.getAge() > 30 && request.getAge() <= 60)) {
-			mismatchCriteriaList.add("Person age should in between 30 to 60");
-		}
-		if (!(request.getYearlyIncome() > 200000)) {
-			mismatchCriteriaList.add("minimum income should be more than 200000");
-		}
-		if (!(request.getCibilScore() > 500)) {
-			mismatchCriteriaList.add("Low CIBIL Score please try after 6 month");
-		}
-
-		if (mismatchCriteriaList.size() > 0) {
-			acknowledgement.setApprovedAmount(0);
-			acknowledgement.setIsEligible(false);
-		} else {
-			acknowledgement.setApprovedAmount(500000);
-			acknowledgement.setIsEligible(true);
-			mismatchCriteriaList.clear();
-		}
-		return acknowledgement;
+    String scenarioType = request.getScenarioType();
+    int firstNumber = request.getFirstNumber();
+    int secondNumber = request.getSecondNumber();
+    Acknowledgement calculationSoapResponsePOJO = new Acknowledgement();
+    calculationSoapResponsePOJO.setFirstNumber(firstNumber);
+    calculationSoapResponsePOJO.setSecondNumber(secondNumber);
+    calculationSoapResponsePOJO.setAddition(firstNumber + secondNumber);
+    calculationSoapResponsePOJO.setSubtraction(firstNumber - secondNumber);
+    calculationSoapResponsePOJO.setMultiplication(firstNumber * secondNumber);
+    calculationSoapResponsePOJO.setDivision(firstNumber / secondNumber);
+    return calculationSoapResponsePOJO;
 
 	}
 
